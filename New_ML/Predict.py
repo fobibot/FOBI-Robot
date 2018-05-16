006b19e71659cd511886cbff154275c119879cbf
Arsapol Manamunchaiyaporn <arsapol@Arsapols-MacBook-Pro.local>
2018-05-16 21:06:21 +0700

    fixed dir bug, clear keras session (maybe load model faster)

--- New_ML/Predict.py
+++ New_ML/Predict.py
@@ -3,6 +3,7 @@ from deepcut import tokenize as word_tokenize
 from sklearn.preprocessing import LabelEncoder
 from keras.models import load_model
 from keras.preprocessing import sequence
+from keras import backend as K
 import pandas
 import numpy as np
 import time as t
@@ -32,6 +33,7 @@ class Prediction:
         print("Loaded Encodeded Output Model :", t.time()-start)
 
         start = t.time()
+        K.clear_session()
         self.model = load_model('New_ML/Saved_Model/model.h5')
         print("Loaded LSTM Model :", t.time()-start)
 
@@ -56,7 +58,7 @@ class Prediction:
             if sentence == "1":
                 sentence = listen_func()
                 last = t.time()
-                words = word_tokenize(sentence, custom_dict="custom_dict.txt") #wait for input sentence by typing
+                words = word_tokenize(sentence, custom_dict="/home/pi/new/thesis/custom_dict.txt") #wait for input sentence by typing
                 print("Word Segmentation time :", t.time()-last)
                 
                 last = t.time()
@@ -76,7 +78,7 @@ class Prediction:
 
     def Predict(self, sentence):
         last = t.time()
-        words = word_tokenize(sentence, custom_dict="custom_dict.txt") #wait for input sentence by typing
+        words = word_tokenize(sentence, custom_dict="/home/pi/new/thesis/custom_dict.txt") #wait for input sentence by typing
         if __debug__:
             print("Word Segmentation time :", t.time()-last)
         
