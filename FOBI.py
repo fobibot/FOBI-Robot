c540ae50f06d47a57a0de184c8e6a3ae17ea7d35
Arsapol Manamunchaiyaporn <arsapol@Arsapols-MacBook-Pro.local>
2018-05-23 03:21:11 +0700

    fixed some bugs in FOBI.py

--- FOBI.py
+++ FOBI.py
@@ -46,7 +46,7 @@ class Robot:
     def LoadRobotMotion(self):
         if os_type == 'Linux': #RPi
             self.Motion = action.action(camera=True)
-        self.Motion.motion("curious") # -> sad, happy, angry, normal, curious
+        self.Motion.motion("normal") # -> sad, happy, angry, normal, curious
 
     def LoadJsonFile(self, filename):
         try:
@@ -76,10 +76,11 @@ class Robot:
         except IndexError:
             print("Something wrong with emotion in \'text.rive\' or \'action.py\'")
             print("Error in SpeakAndReply function : answer = ", answer[0])
-            answer = answer[0]
-            emotion = "normal"
-            if os_type == 'Linux': #RPi
-                self.Motion.motion(emotion)
+            answer = " "
+            # answer = answer[0]
+            # emotion = "normal"
+            # if os_type == 'Linux': #RPi
+            #     self.Motion.motion(emotion)
             
         if answer == "FO BEE":
             self.Speech.Speak(answer, self.english, wait=True, robot_name=True)
